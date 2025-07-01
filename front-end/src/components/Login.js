b257016b17b645a95e307ac007d0e58ab6367385
trantien <tranquyettien09042003@gmail.com>
2025-07-01 22:08:26 +0700

    fix

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -102,7 +102,7 @@ const Login = () => {
                 href="#!"
                 role="button"
               >
-                <i className="fab fa-twitter me-2"></i>Continue with Twitter
+                <i className="fab fa-twitter me-2"></i>Continue with TwitterSSSSSSS
               </a>
             </form>
           </div>
