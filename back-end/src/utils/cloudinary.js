2b93d0cd00f8ab7ca96a1ae1d582e1c9314087ec
linh3031844 <nguyendanglinh31102003@gmail.com>
2025-07-15 13:58:47 +0700

    linhnd

---
+++ back-end/src/utils/cloudinary.js
@@ -0,0 +1,21 @@
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+
+cloudinary.config({
+    cloud_name: 'dzkl4pzbi',
+    api_key: '326717636746418',
+    api_secret: '0qBo4P6os6wOWY05M7g4zvRNuDA',
+});
+x
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: {
+        folder: 'products',
+        allowed_formats: ['jpg', 'jpeg', 'png', 'webp'],
+    },
+});
+
+const upload = multer({ storage });
+
+module.exports = { cloudinary, upload };
